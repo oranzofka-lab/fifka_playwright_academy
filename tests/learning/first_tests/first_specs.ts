90f4e58989dd1716f51feffe996549ea75264096
oranzofka <oranzofka@gmail.com>
2025-11-10 17:27:00 +0100

    Playwright init an d create first test, modify actions config to run only 1 test

---
+++ tests/learning/first_tests/first_specs.ts
@@ -0,0 +1,9 @@
+import { test } from "@playwright/test";
+
+// ? Snippet: pwt (pw-test)
+test("First test", async ({ page }) => {});
+
+await page.goto("https://tredgate.com/pmtool");
+await page.locator("#username").fill("pw_academy");
+await page.locator("#password").fill("Playwright321!");
+await page.locator('[type="submit"]').click();
