cb580157286938b6712b2b7d4c18734a9c2888a2
oranzofka <oranzofka@gmail.com>
2025-11-10 17:35:20 +0100

    oprava

--- tests/learning/first_tests/first_specs.ts
+++ tests/learning/first_tests/first_specs.ts
@@ -1,9 +1,10 @@
 import { test } from "@playwright/test";
 
 // ? Snippet: pwt (pw-test)
-test("First test", async ({ page }) => {});
-
-await page.goto("https://tredgate.com/pmtool");
-await page.locator("#username").fill("pw_academy");
-await page.locator("#password").fill("Playwright321!");
-await page.locator('[type="submit"]').click();
+test("First Test", async ({ page }) => {
+  // ? testovací kroky
+  await page.goto("https://tredgate.com/pmtool");
+  await page.locator("#username").fill("pw_academy");
+  await page.locator("#password").fill("Playwright321!");
+  await page.locator('[type="submit"]').click();
+});
