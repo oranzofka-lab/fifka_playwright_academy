2d74c95b267007cd6566731249599ad8e6a92cfb
oranzofka <oranzofka@gmail.com>
2025-11-14 14:30:28 +0100

    Domaci ukol TC playwright

---
+++ tests/homework/hw_intro_playwright.spec.ts
@@ -0,0 +1,10 @@
+import { test } from "@playwright/test";
+import { title } from "process";
+
+test("Vlozeni do kosiku", async ({ page }) => {
+  await page.goto("https://tredgate.com/eshop/");
+  await page.locator("#search input").fill("iphone");
+  await page.locator('#search [type="button"]').click();
+  await page.getByRole("link", { name: "iPhone" }).first().click();
+  await page.locator("#button-cart").click();
+});
